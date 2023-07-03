40f721f4d25bd723b27de6fa56ed2c4e726be01f
jay ess <mentoce@gmail.com>
2023-07-02 20:14:14 -0700

    Start work on lists' models

--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class List(models.Model):
+    title = models.CharField(max_length=150, blank=False, null=False)
+    owner = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, related_name="lists"
+    )
+
+    def __str__(self) -> str:
+        return f"{self.title} ({self.owner})"
+
+
+class ListItem(models.Model):
+    completed = models.BooleanField(default=False, null=False, blank=False)
+    text = models.TextField(blank=False, null=False)
+    list = models.ForeignKey(List, on_delete=models.CASCADE, related_name="items")
+
+    def __str__(self) -> str:
+        return self.text[:50]
