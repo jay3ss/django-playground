06ab9e74b03619f63f488d8412221659adc0a073
jay ess <mentoce@gmail.com>
2023-07-03 01:23:48 -0700

    Add views for individual and all lists

--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic import DetailView, ListView
 
-# Create your views here.
+from .models import List, ListItem
+
+
+class ListDetailView(DetailView):
+    model = List
+    template_name = "lists/detail.html"
+
+
+class ListIndexView(ListView):
+    model = List
+    template_name = "lists/index.html"
