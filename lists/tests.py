ec11904ec4be83b096a76afd4b797a8bece0cedf
jay ess <mentoce@gmail.com>
2023-07-03 00:18:28 -0700

    Add basic tests for List and ListItem models

--- lists/tests.py
+++ lists/tests.py
@@ -28,6 +28,7 @@ class TestList(TestCase):
             owner=self.user, title="Another Test List", is_public=True
         )
         self.assertEqual(l.owner, self.user)
+        self.assertEqual(self.user.lists.first(), l)
         self.assertEqual(l.title, "Another Test List")
         self.assertTrue(l.is_public)
         self.assertEqual(l.items.count(), 0)
@@ -42,3 +43,34 @@ class TestList(TestCase):
         self.assertTrue(l.is_public)
         self.assertEqual(l.items.count(), 1)
         self.assertEqual(l.items.first().text, "A list item")
+
+
+class TestListItem(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(
+            username="test",
+            email="test@example.com",
+        )
+        cls.list = List.objects.create(
+            owner=cls.user,
+            title="A Test List",
+        )
+
+    def test_list_item_creation_with_defaults(self):
+        li = ListItem.objects.create(
+            text="A list item",
+            list=self.list,
+        )
+        self.assertEqual(li.list.owner, self.user)
+        self.assertEqual(li.list, self.list)
+        self.assertEqual(li.text, "A list item")
+        self.assertFalse(li.is_complete)
+
+    def test_list_item_creation_with_set_values(self):
+        li = ListItem.objects.create(
+            text="A list item", list=self.list, is_complete=True
+        )
+        self.assertEqual(li.list.owner, self.user)
+        self.assertEqual(li.text, "A list item")
+        self.assertTrue(li.is_complete)
