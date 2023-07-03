f24a96d84698eaa8c59204d36352468bc474c470
jay ess <mentoce@gmail.com>
2023-07-03 01:22:47 -0700

    Add lists app's URLs to the core app

--- core/urls.py
+++ core/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", TemplateView.as_view(template_name="index.html")),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("lists/", include("lists.urls")),
 ]
