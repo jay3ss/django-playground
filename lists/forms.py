74bbf6acf4aeb008e64e69052cabf5014022ba9d
jay ess <mentoce@gmail.com>
2023-07-03 22:55:26 -0700

    Add edit template for lists

---
+++ lists/forms.py
@@ -0,0 +1,12 @@
+from django.forms import ModelForm, inlineformset_factory
+
+from .models import List, ListItem
+
+
+class ListForm(ModelForm):
+    class Meta:
+        model = List
+        exclude = ("owner",)
+
+
+ListItemInlineFormset = inlineformset_factory(List, ListItem, fields="__all__", extra=5)
