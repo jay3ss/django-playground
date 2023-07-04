4ece02795d8f076bd60c50fbaf13fb600f8300d4
jay ess <mentoce@gmail.com>
2023-07-03 18:29:08 -0700

    Add custom context object name

--- lists/views.py
+++ lists/views.py
@@ -6,8 +6,10 @@ from .models import List, ListItem
 class ListDetailView(DetailView):
     model = List
     template_name = "lists/detail.html"
+    context_object_name = "list_obj"
 
 
 class ListIndexView(ListView):
     model = List
     template_name = "lists/index.html"
+    context_object_name = "lists"
