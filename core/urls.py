e67d49597c698d85466d96f6c65b720781ba4369
jay ess <mentoce@gmail.com>
2023-07-03 21:38:39 -0700

    Add name for home page route

--- core/urls.py
+++ core/urls.py
@@ -20,7 +20,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", TemplateView.as_view(template_name="index.html")),
+    path("", TemplateView.as_view(template_name="index.html"), name="home"),
     path("accounts/", include("django.contrib.auth.urls")),
     path("lists/", include("lists.urls")),
 ]
