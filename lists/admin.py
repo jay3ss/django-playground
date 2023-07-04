9f418659904805861966e109b9598f7992fd9fbb
jay ess <mentoce@gmail.com>
2023-07-03 18:25:05 -0700

    Register models with admin

--- lists/admin.py
+++ lists/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import List, ListItem
+
+
+class ListItemInline(admin.TabularInline):
+    model = ListItem
+
+
+class ListAdmin(admin.ModelAdmin):
+    inlines = [
+        ListItemInline,
+    ]
+
+
+admin.site.register(List, ListAdmin)
