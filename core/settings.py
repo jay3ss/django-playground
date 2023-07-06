7a573457e51a988d9fff27a3aac4a6decc22cb8a
jay ess <mentoce@gmail.com>
2023-07-05 19:05:59 -0700

    Migrate django allauth

--- core/settings.py
+++ core/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     # third party
     "crispy_forms",
     "crispy_bootstrap5",
+    "allauth",
+    "allauth.account",
     # local
     "accounts.apps.AccountsConfig",
     "lists.apps.ListsConfig",
@@ -135,3 +137,16 @@ AUTH_USER_MODEL = "accounts.CustomUser"
 # crispy forms
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+# django allauth
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
+ACCOUNT_LOGOUT_REDIRECT = "home"
+
+SITE_ID = 1
+AUTHENTICATION_BACKENDS = (
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
+)
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
